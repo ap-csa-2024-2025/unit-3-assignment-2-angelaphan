0686090b6205c28b29ac8f5d1fc24dfcfaaf86ed
angelaphan <aphan117@student.hbuhsd.edu>
2024-10-25 10:17:09 -0700

    b

--- Main.java
+++ Main.java
@@ -4,6 +4,30 @@ public class Main
 {
   public static void main(String[] args)
   {
+    problem1();
+    problem2();
+
     // write solutions to problems here
+  }
+  public static void problem1() 
+  {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("enter a double ");
+    double num = scan.nextDouble();
+    if (num == 12.345)
+    {
+      System.out.println("YES");
+    }
+  }
+
+  public static void problem2() 
+  {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("enter a int ");
+    int num = scan.nextInt();
+    if (num == 48)
+    {
+      System.out.println("YES");
+    }
   }
 }
